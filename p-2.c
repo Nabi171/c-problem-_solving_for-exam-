85bfb8a10800001d8f38394748ae49af7b42807f
Md Nabiul Bashar <nabiultasbed@gmail.com>
2023-01-27 22:14:41 +0600

    changes in some pushing

---
+++ p-2.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+void main()
+{
+    float first, second;
+    scanf("%f, %f", &first, &second);
+    if (first = second)
+    {
+        printf("both numbers are equal");
+    }
+    else
+    {
+        printf("Both numbers are not equal");
+    }
+}
